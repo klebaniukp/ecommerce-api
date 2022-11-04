61a0460bf61a2d59cfc6c4f5fa43dd5f25db37c5
klebaniukp <klebaniukp@gmail.com>
2022-11-04 12:45:49 +0100

    naming edit

--- src/main/java/com/ecommerce/ecommerceapi/User/User.java
+++ src/main/java/com/ecommerce/ecommerceapi/User/User.java
@@ -34,7 +34,7 @@ public class User {
         }
     }
 
-    @GetMapping(path = "/get/{name}")
+    @GetMapping(path = "/get/byName/{name}")
     public List<UserEntity> getByName(@PathVariable String name) {
         return userRepository.findByName(name);
     }
